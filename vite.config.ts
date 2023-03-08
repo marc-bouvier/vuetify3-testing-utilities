27152e875000c3639bcf660d20026a81649e54d6
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-03-08 16:46:27 +0100

    fix ts issue with vuetify vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -8,7 +8,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [vue(), vuetify({ autoImport: true }) as PluginOption],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
