8c2d22bb8b0f41a7a1dd4729fd09d0ddf42c8f24
Marc Bouvier <m.bouvier.dev@gmail.com>
2023-03-08 14:53:45 +0100

    dialog

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
+    {
+      path: "/dialog",
+      name: "dialog",
+      component: () => import("../views/PageWithDialogView.vue"),
+    },
   ],
 });
 
---
+++ src/views/PageWithDialogView.vue
@@ -0,0 +1,6 @@
+<template>
+  <ADialog />
+</template>
+<script setup lang="ts">
+import ADialog from "@/components/ADialog.vue";
+</script>
